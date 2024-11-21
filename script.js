e9be3114c6df36de97f28ce294f0c33f25f101c5
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-11-21 20:38:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,46 +5,33 @@ const name = document.getElementById("name");
 const btn = document.getElementById("btn");
 const form = document.getElementById("form")
 
-
-
 btn.addEventListener('click', () => {
 	
 	let ageValue= age.value;
 	let nameValue = name.value;
-	
-	let boolValue = validation();
-	
-	let promise = new Promise(function(resolve, reject){
-		if(boolValue){
-			resolve("Resolved");
-		}
-		else{
-			reject("Rejected");
-		}
-	}).then(function(message){
-		if(ageValue>=18){
-			alert("Welcome, "+nameValue+". You can vote.");
-		}
-		else{
-			alert("Oh sorry "+nameValue+". You aren't old enough.");
-		}
-		
-	}).catch(function(error){
-		console.log(error);
-	});
-})
 
-
-
-
-const validation = () => {
-	let ageValue= age.value;
-	let nameValue = name.value;
 	if(!ageValue || !nameValue){
 		alert("inputs cannot be empty");
-		return false;
 	}
 	else{
-		return true;
+		let agepromise = new Promise(function(resolve, reject){
+			if(ageValue>=18){
+				resolve("Resolved");
+			}
+			else{
+				reject("Rejected");
+			}
+		}).then(resolve = () => {
+			alert("Welcome, "+nameValue+". You can vote.");
+		}).catch(reject = () => {
+			alert("Oh sorry "+nameValue+". You aren't old enough");
+		});
 	}
-}
+
+	
+	
+	
+	
+
+});
+
